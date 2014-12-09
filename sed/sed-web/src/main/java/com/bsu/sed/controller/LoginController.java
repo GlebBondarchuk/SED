416520b5eab7b822ff0ed721e475efeb6783fae2
Gleb <gleb.exadel@mail.ru>
2014-12-09 19:42:27 +0400

    Added Tile Definitions

--- sed/sed-web/src/main/java/com/bsu/sed/controller/LoginController.java
+++ sed/sed-web/src/main/java/com/bsu/sed/controller/LoginController.java
@@ -1,12 +1,20 @@
 package com.bsu.sed.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Controller for Login page
  *
  * @author gbondarchuk
  */
-@Controller("/login")
+@Controller
 public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView("sed-main");
+        return modelAndView;
+    }
 }
--- sed/sed-web/src/main/webapp/WEB-INF/view/layout/footer.jsp
+++ sed/sed-web/src/main/webapp/WEB-INF/view/layout/footer.jsp
@@ -1,9 +1,10 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <html>
 <head>
     <title></title>
 </head>
 <body>
-
+   <spring:message code="label.title"/>
 </body>
 </html>
