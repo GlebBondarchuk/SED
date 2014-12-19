12feddc70e5bbbe22c7cc310a9d3f2653257fbc8
Gleb <gleb.exadel@mail.ru>
2014-12-19 14:07:19 +0400

    Integration tests added. Mail module added.

--- sed/sed-web/src/main/java/com/bsu/sed/exception/HttpStatusExceptionHandler.java
+++ sed/sed-web/src/main/java/com/bsu/sed/exception/HttpStatusExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.bsu.sed.controller.exception;
+package com.bsu.sed.exception;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
